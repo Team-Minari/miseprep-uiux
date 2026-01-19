091b7378ef76143dbfbfd1a5d4327ed2bde90d51
Eunhyeon Kang <kaeuhy_0315@naver.com>
2026-01-19 22:03:49 +0900

    #1 chore: 초기 개발 환경 구축 - 개발 환경 구축   - react 프로젝트 생성   - 필요 라이브러리 install   - tanstack query 설정   - react router 설정
    Resolves: #1
    See also: None

---
+++ src/router/AppRouter.tsx
@@ -0,0 +1,12 @@
+import { Routes, Route } from "react-router-dom";
+import MainPage from "../pages/MainPage.tsx";
+
+export default function AppRouter() {
+	return (
+		<>
+			<Routes>
+				<Route path="/" element={<MainPage />} />
+			</Routes>
+		</>
+	);
+}
