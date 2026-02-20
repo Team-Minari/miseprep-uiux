a7078ad6d11a0f933bc8ff67a4b318e29b26b429
kaeuhy <kaeuhy_0315@naver.com>
2026-02-20 20:27:53 +0900

    hotfix: lint 의존성 충돌 해결

--- eslint.config.js
+++ eslint.config.js
@@ -4,6 +4,8 @@ import reactHooks from 'eslint-plugin-react-hooks'
 import reactRefresh from 'eslint-plugin-react-refresh'
 import tseslint from 'typescript-eslint'
 import { globalIgnores } from 'eslint/config'
+import pluginQuery from '@tanstack/eslint-plugin-query'
+import prettier from 'eslint-config-prettier'
 
 export default tseslint.config([
   globalIgnores(['dist']),
@@ -14,10 +16,19 @@ export default tseslint.config([
       tseslint.configs.recommended,
       reactHooks.configs['recommended-latest'],
       reactRefresh.configs.vite,
+      ...pluginQuery.configs['flat/recommended'],
+      prettier,
     ],
     languageOptions: {
       ecmaVersion: 2020,
       globals: globals.browser,
+    },
+    rules: {
+      // 사용하지 않는 변수 경고 (언더스코어 prefix 허용)
+      '@typescript-eslint/no-unused-vars': [
+        'warn',
+        { argsIgnorePattern: '^_', varsIgnorePattern: '^_' },
+      ],
     },
   },
 ])
