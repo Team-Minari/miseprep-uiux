04f1a9c7d464bdc8674438fa6c95a503f8f4ee8c
kaeuhy <kaeuhy_0315@naver.com>
2026-03-03 01:07:52 +0900

    #17 refactor: 전역 ModalProvider로 포털 기반 렌더링 및 지연 로딩 적용 - index.html에 #modal-root 추가, DOM 계층 분리 - src/providers/ModalProvider.tsx 신규 생성   - createPortal + lazy + Suspense + Zustand 조합   - ModalKey 기반 MODAL_MAP으로 모달 확장 용이성 확보 - GlobalLayout.tsx에서 모달 관련 코드 전부 제거 - main.tsx에 ModalProvider 래핑 추가 - useCartModalStore.ts에 ModalKey 타입 및 useModalOpenState 훅 추가
    Resolves: #17
    See also: None

--- src/main.tsx
+++ src/main.tsx
@@ -3,13 +3,16 @@ import "./index.css";
 import App from "./App.jsx";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter } from "react-router-dom";
+import ModalProvider from "./providers/ModalProvider";
 
 const queryClient = new QueryClient();
 
 createRoot(document.getElementById("root")!).render(
 	<BrowserRouter>
 		<QueryClientProvider client={queryClient}>
-			<App />
+			<ModalProvider>
+				<App />
+			</ModalProvider>
 		</QueryClientProvider>
 	</BrowserRouter>
 );
